fcf5552eb087cc031e3b98646685ef1c1e0a4d9b
Davide de Caires <work.immedia@davide.co.za>
2017-07-27 13:17:16 +0200

    [SSD-53] Created SuperSportDataEngine.Application.Service.InboundIngestServer project.

---
+++ SuperSportDataEngine.Application.Service.InboundIngestServer/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperSportDataEngine.Application.Service.InboundIngestServer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
