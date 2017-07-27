c343661bf70a8679a5b97d8a1688dd32b510962c
Davide de Caires <work.immedia@davide.co.za>
2017-07-27 13:12:20 +0200

    [SSD-53] Created SuperSportDataEngine.Application.Service.SchedulerIngestServer project.

---
+++ SuperSportDataEngine.Application.Service.SchedulerIngestServer/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperSportDataEngine.Application.Service.SchedulerIngestServer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
