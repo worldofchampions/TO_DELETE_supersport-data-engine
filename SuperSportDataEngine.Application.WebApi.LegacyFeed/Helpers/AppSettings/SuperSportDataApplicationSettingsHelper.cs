f334fab340790f8968034ac924efbd255b46f7bc
Mbuso Mkhize <mbuso@immedia.co.za>
2017-08-02 13:26:29 +0200

    [SSD-71] Create a handler for the request pass through

---
+++ SuperSportDataEngine.Application.WebApi.LegacyFeed/Helpers/AppSettings/SuperSportDataApplicationSettingsHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+namespace SuperSportDataEngine.Application.WebApi.LegacyFeed.Helpers.AppSettings
+{
+    public static class SuperSportDataApplicationSettingsHelper
+    {
+        private const string SUPERSPORTFEED_HOST = "SuperSportFeed.IpAddress";
+
+        public static string GetSuperSportFeedHost(string defaultValue = null)
+        {
+            var superSportFeedHost = ConfigurationManager.AppSettings[SUPERSPORTFEED_HOST];
+
+            if (string.IsNullOrWhiteSpace(superSportFeedHost) && !string.IsNullOrWhiteSpace(defaultValue))
+                superSportFeedHost = defaultValue;
+
+            return superSportFeedHost;
+        }
+    }
+
+    
+    
+}
