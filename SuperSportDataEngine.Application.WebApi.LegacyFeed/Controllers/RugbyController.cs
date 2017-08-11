3abcd55faefde32708688e1a70145ebbf41b1594
Mbuso Mkhize <mbuso@immedia.co.za>
2017-08-11 15:52:56 +0200

    Create RugbyController

---
+++ SuperSportDataEngine.Application.WebApi.LegacyFeed/Controllers/RugbyController.cs
@@ -0,0 +1,107 @@
+﻿using SuperSportDataEngine.Application.WebApi.LegacyFeed.Models;
+using SuperSportDataEngine.Application.WebApi.LegacyFeed.Models.News;
+using SuperSportDataEngine.Application.WebApi.LegacyFeed.Models.Rugby;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace SuperSportDataEngine.Application.WebApi.LegacyFeed.Controllers
+{
+    /// <summary>
+    /// SuperSport Rugby Endpoints
+    /// </summary>
+    [RoutePrefix("rugby")]
+    public class RugbyController : ApiController
+    {
+        /// <summary>
+        /// Match Details
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("matchdetails/{id:int}")]
+        [ResponseType(typeof(RugbyMatchDetailsModel))]
+        public IHttpActionResult GetMatchDetails(int id)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Get News for Rugby
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("news")]
+        [ResponseType(typeof(NewsModel))]
+        public IHttpActionResult GetNews()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Get Fixtures for Tournament
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{category}/fixtures")]
+        [ResponseType(typeof(List<FixtureModel>))]
+        public IHttpActionResult GetFixtures()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Get Results for Tournament
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{category}/results")]
+        [ResponseType(typeof(List<ResultModel>))]
+        public IHttpActionResult GetResults()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Get Logs for Tournament
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{category}/logs")]
+        [ResponseType(typeof(List<LogModel>))]
+        public IHttpActionResult GetLogs()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Get Tournament News
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{category}/news")]
+        [ResponseType(typeof(NewsModel))]
+        public IHttpActionResult GetTournamentNews()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Get News for specific Tournament
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{category}/news/{id:int}")]
+        [ResponseType(typeof(NewsModel))]
+        public IHttpActionResult GetTournamentNews(int id)
+        {
+            return Ok();
+        }
+
+
+    }
+}
