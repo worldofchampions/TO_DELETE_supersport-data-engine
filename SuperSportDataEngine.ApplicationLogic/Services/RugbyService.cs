44546156fb12e63f413c45dfa7567f63484d4389
Mbuso Mkhize <mbuso@immedia.co.za>
2017-08-11 16:00:27 +0200

    [SSD-79] Add Rugby Service.

---
+++ SuperSportDataEngine.ApplicationLogic/Services/RugbyService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using SuperSportDataEngine.ApplicationLogic.Boundaries.ApplicationLogic.Interfaces;
+using SuperSportDataEngine.ApplicationLogic.Entities;
+
+namespace SuperSportDataEngine.ApplicationLogic.Services
+{
+    public class RugbyService : IRugbyService
+    {
+        public List<LogEntity> GetLogs()
+        {
+            return new List<LogEntity>();
+        }
+    }
+}
