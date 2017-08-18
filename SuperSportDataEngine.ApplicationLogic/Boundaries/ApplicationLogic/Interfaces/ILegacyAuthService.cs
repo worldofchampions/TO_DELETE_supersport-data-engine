b8cc10aa4a4142f449762c87b896cf36593d9731
Mbuso Mkhize <mbuso@immedia.co.za>
2017-08-18 12:33:28 +0200

    Add LegacyAuth

--- SuperSportDataEngine.ApplicationLogic/Boundaries/ApplicationLogic/Interfaces/ILegacyAuthService.cs
+++ SuperSportDataEngine.ApplicationLogic/Boundaries/ApplicationLogic/Interfaces/ILegacyAuthService.cs
@@ -1,13 +1,12 @@
-﻿using System;
+﻿using SuperSportDataEngine.ApplicationLogic.Entities.Legacy;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SuperSportDataEngine.ApplicationLogic.Boundaries.ApplicationLogic.Interfaces
 {
     public interface ILegacyAuthService
     {
-        bool IsAuthorised(string authKey, string siteId = null);
+        bool IsAuthorised(string authKey, int? siteId = null);
+        bool ImportZoneSiteRecords(IEnumerable<LegacyZoneSiteEntity> models);
+        bool ImportAuthFeedRecords(IEnumerable<LegacyAuthFeedConsumerEntity> models);
     }
 }
--- SuperSportDataEngine.ApplicationLogic/Services/LegacyAuthService.cs
+++ SuperSportDataEngine.ApplicationLogic/Services/LegacyAuthService.cs
@@ -1,21 +1,53 @@
 ﻿using SuperSportDataEngine.ApplicationLogic.Boundaries.ApplicationLogic.Interfaces;
 using SuperSportDataEngine.ApplicationLogic.Boundaries.Repository.EntityFramework.Common.Interfaces;
 using SuperSportDataEngine.ApplicationLogic.Boundaries.Repository.EntityFramework.PublicSportData.Models;
+using SuperSportDataEngine.ApplicationLogic.Entities.Legacy;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SuperSportDataEngine.ApplicationLogic.Services
 {
     public class LegacyAuthService : ILegacyAuthService
     {
         private readonly IBaseEntityFrameworkRepository<LegacyZoneSite> _legacyZoneSiteRepository;
-        public LegacyAuthService(IBaseEntityFrameworkRepository<LegacyZoneSite> legacyZoneSiteRepository)
+        private readonly IBaseEntityFrameworkRepository<LegacyAuthFeedConsumer> _legacyAuthFeedConsumerRepository;
+
+        public LegacyAuthService(IBaseEntityFrameworkRepository<LegacyZoneSite> legacyZoneSiteRepository,
+            IBaseEntityFrameworkRepository<LegacyAuthFeedConsumer> legacyAuthFeedConsumerRepository)
         {
             _legacyZoneSiteRepository = legacyZoneSiteRepository;
+            _legacyAuthFeedConsumerRepository = legacyAuthFeedConsumerRepository;
         }
 
-        public bool IsAuthorised(string authKey, string siteId = null)
+        public bool IsAuthorised(string authKey, int? siteId = null)
         {
-            //_legacyZoneSiteRepository.Where
-            return false;
+            var legacyAuthFeed = _legacyAuthFeedConsumerRepository.Where(c => c.AuthKey == authKey).FirstOrDefault();
+            if (legacyAuthFeed == null)
+            {
+                return false;
+            }
+
+            if (siteId != null)
+            {
+                var legacyZone = _legacyZoneSiteRepository.Where(c => c.Id == siteId).FirstOrDefault();
+                return legacyZone.Feed == legacyAuthFeed.Name.Replace("  ", string.Empty).ToLowerInvariant();
+            }
+
+            return true;
+        }
+
+        public bool ImportZoneSiteRecords(IEnumerable<LegacyZoneSiteEntity> models)
+        {
+            //var legacyModels = 
+            //_legacyAuthFeedConsumerRepository.AddRange()
+            return true;
+        }
+
+        public bool ImportAuthFeedRecords(IEnumerable<LegacyAuthFeedConsumerEntity> models)
+        {
+            //var legacyModels = 
+            //_legacyAuthFeedConsumerRepository.AddRange()
+            return true;
         }
     }
 }
