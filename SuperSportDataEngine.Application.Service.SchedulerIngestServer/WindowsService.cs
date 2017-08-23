03a4043efe7a5bca77a74ae18e5d61e4fd31e7ce
Davide de Caires <work.immedia@davide.co.za>
2017-08-23 07:42:59 +0200

    [SSD-81] Integrate Topshelf into Application.Service level projects.

---
+++ SuperSportDataEngine.Application.Service.SchedulerIngestServer/WindowsService.cs
@@ -0,0 +1,50 @@
+﻿namespace SuperSportDataEngine.Application.Service.SchedulerIngestServer
+{
+    using Hangfire;
+    using Hangfire.SqlServer;
+    using Microsoft.Practices.Unity;
+    using SuperSportDataEngine.Application.Service.Common.Hangfire.Filters;
+    using SuperSportDataEngine.Application.Service.Common.Interfaces;
+    using SuperSportDataEngine.ApplicationLogic.Services;
+    using System;
+    using System.Configuration;
+
+    internal class WindowsService : IWindowsServiceContract
+    {
+        private readonly UnityContainer _container;
+
+        public WindowsService(UnityContainer container)
+        {
+            _container = container;
+        }
+
+        public void StartService()
+        {
+            SqlServerStorageOptions Options = new SqlServerStorageOptions { PrepareSchemaIfNecessary = true };
+
+            JobStorage JOB_STORAGE =
+            new SqlServerStorage(
+                    ConfigurationManager.ConnectionStrings["SqlDatabase_Hangfire"].ConnectionString,
+                    Options
+                );
+
+            var ingestService = _container.Resolve<IRugbyIngestWorkerService>();
+
+            GlobalConfiguration.Configuration.UseStorage(JOB_STORAGE);
+            GlobalJobFilters.Filters.Add(new ExpirationTimeAttribute());
+
+            using (var server = new BackgroundJobServer())
+            {
+                JobStorage.Current = JOB_STORAGE;
+                // Processing of jobs happen here.
+
+                Console.ReadLine();
+            }
+        }
+
+        public void StopService()
+        {
+            // TODO: Implement resource disposal/clean-up here.
+        }
+    }
+}
