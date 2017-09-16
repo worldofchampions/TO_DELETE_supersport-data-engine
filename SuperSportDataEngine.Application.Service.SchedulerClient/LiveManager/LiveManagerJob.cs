abc64978514352e38edd2309c02e77a92164f4d2
Ronald Mariah <ronald@immedia.co.za>
2017-09-16 22:19:40 +0200

    Added in base code for LiveJobManager. This class will be responsible for creating and deleting child jobs that run for pulling live data from the provider for games that are currently being played

---
+++ SuperSportDataEngine.Application.Service.SchedulerClient/LiveManager/LiveManagerJob.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Practices.Unity;
+using SuperSportDataEngine.ApplicationLogic.Boundaries.ApplicationLogic.Interfaces;
+using SuperSportDataEngine.ApplicationLogic.Services;
+using System;
+using System.Timers;
+
+namespace SuperSportDataEngine.Application.Service.SchedulerClient.LiveManager
+{
+    internal class LiveManagerJob
+    {
+        private System.Timers.Timer _timer;
+        private IRugbyService _rugbyService;
+        private IRugbyIngestWorkerService _rugbyIngestService;
+
+        public LiveManagerJob(UnityContainer container)
+        {
+            // This timer will run on a separate thread.
+            // Every minute a method will be called.
+            _timer = new System.Timers.Timer
+            {
+                AutoReset = false,
+                Interval = TimeSpan.FromMinutes(1).TotalMilliseconds
+            };
+
+            _timer.Elapsed += new ElapsedEventHandler(UpdateManagerJobs);
+            _timer.Start();
+
+            _rugbyService = container.Resolve<IRugbyService>();
+            _rugbyIngestService = container.Resolve<IRugbyIngestWorkerService>();
+        }
+
+        private async void UpdateManagerJobs(object sender, ElapsedEventArgs e)
+        {
+            _timer.Start();
+        }
+    }
+}
