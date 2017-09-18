b0449c43eee4930692ad0f72c338c1206b537049
Thobani-Madonsela <thobani@immedia.co.za>
2017-09-18 17:36:22 +0200

    Feature: Cleanup Manager For Logs Jobs
    Implement mechanism to delete unwanted logs jobs

--- SuperSportDataEngine.Application.Service.SchedulerClient/ScheduledManager/LogsScheduledManagerJob.cs
+++ SuperSportDataEngine.Application.Service.SchedulerClient/ScheduledManager/LogsScheduledManagerJob.cs
@@ -25,14 +25,12 @@ namespace SuperSportDataEngine.Application.Service.SchedulerClient.ScheduledMana
 
         public override async void UpdateManagerJobs(object sender, ElapsedEventArgs e)
         {
-            await CreateChildJobsForMatchesInResultsState15MinutePollingAsync();
-
-            await CreateChildJobsForMatchesInResultsStateOneMinutePolling();
+            await CreateChildJobsForPollingLogs();
 
             base.ResetTimer();
         }
 
-        private async Task CreateChildJobsForMatchesInResultsStateOneMinutePolling()
+        private async Task CreateChildJobsForPollingLogs()
         {
             var activeTournaments = _rugbyService.GetActiveTournamentsForMatchesInResultsState();
 
@@ -48,26 +46,7 @@ namespace SuperSportDataEngine.Application.Service.SchedulerClient.ScheduledMana
 
                     AddOrUpdateHangfireJob(tournament.ProviderTournamentId, seasonId, jobId, jobCronExpression);
 
-                    await _rugbyService.SetSchedulerStatusPollingForTournamentToRunning(tournament.Id);
-                }
-            }
-        }
-
-        private async Task CreateChildJobsForMatchesInResultsState15MinutePollingAsync()
-        {
-            var activeTournaments = _rugbyService.GetActiveTournamentsForMatchesInResultsState();
-
-            foreach (var tournament in activeTournaments)
-            {
-                int seasonId = _rugbyService.GetSeasonIdForTournament(tournament.Id);
-
-                if (_rugbyService.GetSchedulerStateForManagerJobPolling(tournament.Id) == SchedulerStateForManagerJobPolling.NotRunning)
-                {
-                    var jobId = ConfigurationManager.AppSettings["ScheduleMangerJob_Logs_CurrentTournaments_JobIdPrefix"] + tournament.Name;
-
-                    var jobCronExpression = ConfigurationManager.AppSettings["ScheduleMangerJob_Logs_CurrentTournaments_JobCronExpression_15Minutes"];
-
-                    AddOrUpdateHangfireJob(tournament.ProviderTournamentId, seasonId, jobId, jobCronExpression);
+                    QueueJobForLowFrequencyPolling(tournament.Id, tournament.ProviderTournamentId, seasonId, jobId);
 
                     await _rugbyService.SetSchedulerStatusPollingForTournamentToRunning(tournament.Id);
                 }
@@ -82,6 +61,36 @@ namespace SuperSportDataEngine.Application.Service.SchedulerClient.ScheduledMana
                 jobCronExpression,
                 TimeZoneInfo.Utc,
                 HangfireQueueConfiguration.HighPriority);
+        }
+
+        private void QueueJobForLowFrequencyPolling(Guid tournamentId, int providerTournamentId, int seasonId, string jobId)
+        {
+            string highFreqExpiryFromConfig = ConfigurationManager.AppSettings["ScheduleMangerJob_Logs_CurrentTournaments_HighFrequencyPolling_ExpiryInMinutes"];
+
+            int udpateJobFrequencyOnThisMinute = int.Parse(highFreqExpiryFromConfig);
+
+            var timer = new System.Timers.Timer
+            {
+                AutoReset = false,
+                Interval = TimeSpan.FromMinutes(udpateJobFrequencyOnThisMinute).TotalMilliseconds
+            };
+
+            timer.Elapsed += delegate
+            {
+                string jobExpiryFromConfig = ConfigurationManager.AppSettings["ScheduleMangerJob_Logs_CurrentTournaments_LowFrequencyPolling_ExpiryInMinutes"];
+
+                var deleteJobOnThisMinute = int.Parse(jobExpiryFromConfig);
+
+                var jobCronExpression = ConfigurationManager.AppSettings["ScheduleMangerJob_Logs_CurrentTournaments_LowFrequencyPolling_CronExpression"];
+
+                AddOrUpdateHangfireJob(providerTournamentId, seasonId, jobId, jobCronExpression);
+
+                LogsJobCleanupManager.QueueJobForDeletion(tournamentId, jobId, deleteJobOnThisMinute);
+
+                timer.Stop();
+            };
+
+            timer.Start();
         }
     }
 }
