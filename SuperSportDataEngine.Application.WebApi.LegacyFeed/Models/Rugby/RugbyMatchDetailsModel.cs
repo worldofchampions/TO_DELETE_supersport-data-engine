fa147d0e5d4f94865e35fc2d15e8ac8b03f0a4df
Thobani-Madonsela <thobani@immedia.co.za>
2017-10-09 11:15:40 +0200

    Cleanup: Remove Blank Lines

--- SuperSportDataEngine.Application.WebApi.LegacyFeed/Models/Rugby/RugbyMatchDetailsModel.cs
+++ SuperSportDataEngine.Application.WebApi.LegacyFeed/Models/Rugby/RugbyMatchDetailsModel.cs
@@ -7,15 +7,11 @@ namespace SuperSportDataEngine.Application.WebApi.LegacyFeed.Models.Rugby
     public class RugbyMatchDetailsModel : MatchDetailsModel
     {
         public bool isScoredLive { get; set; }
-
         public List<AlternateCommentsModel> AlternateCommentary { get; set; }
-
         public List<ScorerModel> TeamA_Scorers { get; set; }
         public List<ScorerModel> TeamB_Scorers { get; set; }
-
         public RugbyMatchStatisticsModel MatchStatisticsTeamA { get; set; }
         public RugbyMatchStatisticsModel MatchStatisticsTeamB { get; set; }
-
         public RugbyMatchStatisticsModel TeamAStats { get; set; }
         public RugbyMatchStatisticsModel TeamBStats { get; set; }
     }
--- SuperSportDataEngine.Application.WebApi.LegacyFeed/Models/Shared/MatchDetailsModel.cs
+++ SuperSportDataEngine.Application.WebApi.LegacyFeed/Models/Shared/MatchDetailsModel.cs
@@ -6,7 +6,6 @@ namespace SuperSportDataEngine.Application.WebApi.LegacyFeed.Models
     [Serializable]
     public abstract class MatchDetailsModel
     {
-      
         public string LeagueName { get; set; }
         public int LeagueId { get; set; }
         public string LeagueUrlName { get; set; }
@@ -28,43 +27,32 @@ namespace SuperSportDataEngine.Application.WebApi.LegacyFeed.Models
         public bool MatchCompleted { get; set; }
         public List<string> Channels { get; set; }
         public List<ChannelRegions> channelRegions { get; set; }
-
         public virtual int TeamAScore { get; set; }
         public virtual int TeamBScore { get; set; }
         public int TeamAHalfTimeScore { get; set; }
         public int TeamBHalfTimeScore { get; set; }
-
         public int RoundNumber { get; set; }
         public int MatchNumber { get; set; }
         public string Attendance { get; set; }
         public int ReportId { get; set; }
-
         public string ManOfTheMatch { get; set; }
         public int ManOfTheMatchId { get; set; }
-
         public string Group { get; set; }
-
         public List<MatchEventModel> Events { get; set; }
-
         public virtual List<PlayerModel> Teamsheet { get; set; }
         public virtual List<PlayerModel> TeamATeamsheet { get; set; }
         public virtual List<PlayerModel> TeamBTeamsheet { get; set; }
-
         public virtual List<ScorerModel> TeamAScorers { get; set; }
         public virtual List<ScorerModel> TeamBScorers { get; set; }
-
         public virtual List<SubstituteModel> TeamASubstitutes { get; set; }
         public virtual List<SubstituteModel> TeamBSubstitutes { get; set; }
-
         public virtual List<CardsModel> TeamACards { get; set; }
         public virtual List<CardsModel> TeamBCards { get; set; }
-
         public List<OfficialModel> Officials { get; set; }
         public List<MatchVideoModel> Videos { get; set; }
         public List<MatchLiveVideoModel> LiveVideos { get; set; }
         public List<MatchEventModel> Commentary { get; set; }
         public Boolean Postponed { get; set; }
-        //TODO: GET a serializer or formatter for this field
         public DateTimeOffset MatchTime { get; set; }
     }
 }
