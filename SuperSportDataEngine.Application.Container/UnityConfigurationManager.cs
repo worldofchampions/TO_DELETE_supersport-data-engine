39c96eb17f28b529463bd6721acf8722953c26c0
Ronald Mariah <ronald@immedia.co.za>
2017-11-08 16:09:41 +0200

    Moved registration for Hangfire service after dependencies.

--- SuperSportDataEngine.Application.Container/UnityConfigurationManager.cs
+++ SuperSportDataEngine.Application.Container/UnityConfigurationManager.cs
@@ -54,7 +54,6 @@
         {
             container.RegisterType<ITemporaryExampleService, TemporaryExampleService>();
             container.RegisterType<IRugbyService, RugbyService>(new HierarchicalLifetimeManager());
-            container.RegisterType<IHangfireJobService, HangfireJobService>();
 
             if (applicationScope == ApplicationScope.WebApiLegacyFeed ||
                 applicationScope == ApplicationScope.WebApiSystemApi)
@@ -210,6 +209,7 @@
                 container.RegisterType<ITemporaryExampleMongoDbRepository, TemporaryExampleMongoDbRepository>();
 
                 container.RegisterType<ISchedulerClientService, SchedulerClientService>();
+                container.RegisterType<IHangfireJobService, HangfireJobService>();
             }
         }
     }
