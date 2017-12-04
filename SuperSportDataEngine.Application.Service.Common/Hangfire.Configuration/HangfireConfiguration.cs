9dffbbbb9138cc9073021fd7de2a91bfc9fe6a2d
Ronald Mariah <ronald@immedia.co.za>
2017-12-04 09:25:01 +0200

    Reverting static constructor.

--- SuperSportDataEngine.Application.Service.Common/Hangfire.Configuration/HangfireConfiguration.cs
+++ SuperSportDataEngine.Application.Service.Common/Hangfire.Configuration/HangfireConfiguration.cs
@@ -8,43 +8,92 @@ namespace SuperSportDataEngine.Application.Service.Common.Hangfire.Configuration
 
     public static class HangfireConfiguration
     {
-        static HangfireConfiguration()
+        // Hangfire SQL Server Options.
+        private static SqlServerStorageOptions _storageOptions;
+
+        private static SqlServerStorageOptions StorageOptions
         {
-            ConnectionString =
-                ConfigurationManager
-                    .ConnectionStrings["SqlDatabase_Hangfire"]
-                    .ConnectionString;
-
-            StorageOptions =
-                new SqlServerStorageOptions
+            get
+            {
+                if (_storageOptions == null)
                 {
-                    PrepareSchemaIfNecessary = true
-                };
+                    _storageOptions =
+                        new SqlServerStorageOptions
+                        {
+                            PrepareSchemaIfNecessary = true
+                        };
+                }
 
-            JobStorage =
-                new SqlServerStorage(
-                    ConnectionString,
-                    StorageOptions);
-
-            JobServerOptions =
-                new BackgroundJobServerOptions
-                {
-                    WorkerCount =
-                        Environment.ProcessorCount *
-                        Convert.ToInt32(ConfigurationManager.AppSettings["WorkerCountMultiplier"]),
-                    Queues =
-                        new[] {
-                            HangfireQueueConfiguration.HighPriority,
-                            HangfireQueueConfiguration.NormalPriority }
-                };
+                return _storageOptions;
+            }
         }
 
-        private static SqlServerStorageOptions StorageOptions { get; }
-        
-        private static string ConnectionString { get; }
-        
-        public static JobStorage JobStorage { get; }
-        
-        public static BackgroundJobServerOptions JobServerOptions { get; }
+        // Hangfire SQL Server connection string.
+        private static string _connectionString;
+
+        private static string ConnectionString
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_connectionString))
+                {
+                    _connectionString =
+                        ConfigurationManager
+                            .ConnectionStrings["SqlDatabase_Hangfire"]
+                            .ConnectionString;
+                }
+
+                return _connectionString;
+            }
+        }
+
+        // Hangfire SQL Server storage.
+        private static JobStorage _jobStorage;
+
+        public static JobStorage JobStorage
+        {
+            get
+            {
+                if (_jobStorage == null)
+                {
+                    _jobStorage =
+                        new SqlServerStorage(
+                            ConnectionString,
+                            StorageOptions);
+                }
+
+                return _jobStorage;
+            }
+        }
+
+        // Hangfire Queues options.
+        private static BackgroundJobServerOptions _jobServerOptions;
+
+        public static BackgroundJobServerOptions JobServerOptions
+        {
+            get
+            {
+                if (_jobServerOptions == null)
+                {
+                    _jobServerOptions =
+                        new BackgroundJobServerOptions
+                        {
+                            WorkerCount =
+                                Environment.ProcessorCount *
+                                Convert.ToInt32(ConfigurationManager.AppSettings["WorkerCountMultiplier"]),
+                            Queues =
+                                new[] {
+                                    // The default queue is added first because
+                                    // Hangfire places jobs in this queue when it gets retried.
+                                    // This will be the highest priority queue.
+                                    // In code, no jobs will be scheduled on this queue.
+                                    HangfireQueueConfiguration.HighPriority,
+                                    HangfireQueueConfiguration.NormalPriority }
+                        };
+                }
+
+                return _jobServerOptions;
+            }
+        }
     }
 }
