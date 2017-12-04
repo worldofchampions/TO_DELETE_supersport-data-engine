daf0eb7a7e1dbac0907499ce2c51acc169a60fb1
Thobani Immedia <thobani@immedia.co.za>
2017-12-04 09:21:02 +0200

    Refactor: Removed Unused Code

--- SuperSportDataEngine.ApplicationLogic/Services/RugbyIngestWorkerService.cs
+++ SuperSportDataEngine.ApplicationLogic/Services/RugbyIngestWorkerService.cs
@@ -1505,7 +1505,6 @@
                     RugbyTeam = team,
                     RugbyTeamId = team.Id,
                     YellowCards = (int)statsMap.GetValueOrDefault(2),
-                    //CarriesCrossedGainLine = statsMap.GetValueOrDefault("(Time)Territory"),
                     CleanBreaks = (int)statsMap.GetValueOrDefault(7),
                     ConversionAttempts = (int)statsMap.GetValueOrDefault(2047),
                     Conversions = (int)statsMap.GetValueOrDefault(9),
