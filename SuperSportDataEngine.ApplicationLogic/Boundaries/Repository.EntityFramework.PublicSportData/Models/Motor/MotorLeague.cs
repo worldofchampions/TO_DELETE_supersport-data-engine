8a5520858bc86ebb9b3ed2ce7b2a8a95ad132a1a
Thobani Immedia <thobani@immedia.co.za>
2017-12-07 15:35:47 +0200

    Motor: Added DB Modeles

---
+++ SuperSportDataEngine.ApplicationLogic/Boundaries/Repository.EntityFramework.PublicSportData/Models/Motor/MotorLeague.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using SuperSportDataEngine.ApplicationLogic.Boundaries.Repository.EntityFramework.Common.Models.Enums;
+
+namespace SuperSportDataEngine.ApplicationLogic.Boundaries.Repository.EntityFramework.PublicSportData.Models
+{
+    public class MotorLeague
+    {
+        /// <summary> The primary internal record identifier. </summary>
+        public Guid Id { get; set; }
+
+        /// <summary> A unique int record identifier for legacy purposes. </summary>
+        public int LegacyLeagueId { get; set; }
+
+        /// <summary> The provider's record identifier. </summary>
+        public int ProviderLeagueId { get; set; }
+
+        /// <summary> A provider driven value. </summary>
+        public string Name { get; set; }
+
+        /// <summary> A provider driven value. </summary>
+        public string ProviderSlug { get; set; }
+
+        /// <summary> A CMS driven value. </summary>
+        public string NameCmsOverride { get; set; }
+
+        /// <summary> A CMS defined value to uniquely identify a league for URL purposes. </summary>
+        public string Slug { get; set; }
+
+        /// <summary> A provider driven value. </summary>
+        public string Abbreviation { get; set; }
+
+        /// <summary> A provider driven value. </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary> A CMS driven value. </summary>
+        public string DisplayNameCmsOverride { get; set; }
+
+        /// <summary> A CMS driven value to indicate if a league is enabled for ingest. </summary>
+        public bool IsEnabled { get; set; }
+
+        public DataProvider DataProvider { get; set; }
+    }
+}
