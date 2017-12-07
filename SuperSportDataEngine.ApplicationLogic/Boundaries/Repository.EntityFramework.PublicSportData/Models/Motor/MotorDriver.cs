1cb0d831bac20a6f2f65c5f403471a28052ea762
Thobani Immedia <thobani@immedia.co.za>
2017-12-07 15:35:47 +0200

    Motor: Added DB Models :)

---
+++ SuperSportDataEngine.ApplicationLogic/Boundaries/Repository.EntityFramework.PublicSportData/Models/Motor/MotorDriver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using SuperSportDataEngine.ApplicationLogic.Boundaries.Repository.EntityFramework.Common.Models.Base;
+using SuperSportDataEngine.ApplicationLogic.Boundaries.Repository.EntityFramework.Common.Models.Enums;
+
+namespace SuperSportDataEngine.ApplicationLogic.Boundaries.Repository.EntityFramework.PublicSportData.Models
+{
+    public class MotorDriver: BaseModel
+    {
+        /// <summary> The primary internal record identifier. </summary>
+        public Guid Id { get; set; }
+
+        /// <summary> A unique int record identifier for legacy purposes. </summary>
+        public int LegacyDriverId { get; set; }
+
+        /// <summary> The provider's record identifier. </summary>
+        public int ProviderDriverId { get; set; }
+
+        /// <summary> A provider driven value. </summary>
+        public string FirstName { get; set; }
+
+        /// <summary> A provider driven value. </summary>
+        public string LastName { get; set; }
+
+        /// <summary> Provider does not serve this value. </summary>
+        public string FullName { get; set; }
+
+        /// <summary> A provider driven value. </summary>
+        public double HeightInCentimeters { get; set; }
+
+        /// <summary> A provider driven value. </summary>
+        public double WeightInKilograms { get; set; }
+
+        /// <summary> A CMS driven value. </summary>
+        public string DisplayNameCmsOverride { get; set; }
+
+        public DataProvider DataProvider { get; set; }
+    }
+}
---
+++ SuperSportDataEngine.ApplicationLogic/Boundaries/Repository.EntityFramework.PublicSportData/Models/Motor/MotorRace.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using SuperSportDataEngine.ApplicationLogic.Boundaries.Repository.EntityFramework.Common.Models.Base;
+using SuperSportDataEngine.ApplicationLogic.Boundaries.Repository.EntityFramework.Common.Models.Enums;
+
+namespace SuperSportDataEngine.ApplicationLogic.Boundaries.Repository.EntityFramework.PublicSportData.Models
+{
+    public class MotorRace: BaseModel
+    {
+        /// <summary> The primary internal record identifier. </summary>
+        public Guid Id { get; set; }
+
+        /// <summary> A clustered-key record identifier. </summary>
+        public Guid MotorLeagueId { get; set; }
+
+        /// <summary> A unique int record identifier for legacy purposes. </summary>
+        public int LegacyRaceId { get; set; }
+
+        /// <summary> The provider's record identifier. </summary>
+        public int ProviderRaceId { get; set; }
+
+        /// <summary> A provider driven value. </summary>
+        public string Name { get; set; }
+
+        /// <summary> A CMS driven value. </summary>
+        public string NameCmsOverride { get; set; }
+
+        /// <summary> A CMS defined value to uniquely identify a tournament for URL purposes. </summary>
+        public string Slug { get; set; }
+
+        /// <summary> A provider driven value. </summary>
+        public string Abbreviation { get; set; }
+
+        /// <summary> A CMS driven value. </summary>
+        public string DisplayNameCmsOverride { get; set; }
+
+        public virtual MotorLeague MotorLeague { get; set; }
+
+        public DataProvider DataProvider { get; set; }
+    }
+}
