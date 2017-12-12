d8c62be599256b2946f636decc492896149c916a
Ronald Mariah <ronald@immedia.co.za>
2017-12-12 10:15:29 +0200

    Log the cacheData to file instead of Slack.

--- SuperSportDataEngine.Application.WebApi.LegacyFeed/Controllers/RugbyController.cs
+++ SuperSportDataEngine.Application.WebApi.LegacyFeed/Controllers/RugbyController.cs
@@ -363,7 +363,8 @@ namespace SuperSportDataEngine.Application.WebApi.LegacyFeed.Controllers
             {
                 var loggerService = ActionContext.Request.GetDependencyScope().GetService(typeof(ILoggingService)) as ILoggingService;
 
-                loggerService?.Error("PersistToCache." + cacheKey, "key = " + cacheKey + " cacheData = " + cacheData + " " + exception.Message + exception.StackTrace);
+                loggerService?.Error("PersistToCache." + cacheKey, "key = " + cacheKey + " " + exception.Message + exception.StackTrace);
+                loggerService?.Debug("PersistToCache.CacheData." + cacheKey, "cacheData = " + cacheData);
             }
         }
 
