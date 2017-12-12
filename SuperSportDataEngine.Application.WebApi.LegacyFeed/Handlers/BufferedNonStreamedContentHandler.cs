87f2eff65977da55790a50707434f0bab4fee01b
Ronald Mariah <ronald@immedia.co.za>
2017-12-12 14:57:20 +0200

    Buffer the output before sending it to the client.

---
+++ SuperSportDataEngine.Application.WebApi.LegacyFeed/Handlers/BufferedNonStreamedContentHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Hosting;
+
+namespace SuperSportDataEngine.Application.WebApi.LegacyFeed.Handlers
+{
+    public class BufferNonStreamedContentHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            var response = await base.SendAsync(request, cancellationToken);
+            if (response.Content == null) return response;
+
+            var services = request.GetConfiguration().Services;
+            var bufferPolicy = (IHostBufferPolicySelector)services.GetService(typeof(IHostBufferPolicySelector));
+
+            // If the host is going to buffer it anyway
+            if (bufferPolicy.UseBufferedOutputStream(response))
+            {
+                // Buffer it now so we can catch the exception
+                await response.Content.LoadIntoBufferAsync();
+            }
+            return response;
+        }
+    }
+}
