ed528e6f1a1768b56edd93de46cd22e785ab4e59
Thobani_Madonsela <thobani@immedia.co.za>
2018-01-04 15:14:08 +0200

    Motor: Refactor

--- SuperSportDataEngine.Repository.EntityFramework.PublicSportData/Context/PublicSportDataContext.cs
+++ SuperSportDataEngine.Repository.EntityFramework.PublicSportData/Context/PublicSportDataContext.cs
@@ -30,7 +30,7 @@ namespace SuperSportDataEngine.Repository.EntityFramework.PublicSportData.Contex
         public DbSet<RugbyTournament> RugbyTournaments { get; set; }
         public DbSet<RugbyVenue> RugbyVenues { get; set; }
         public DbSet<MotorDriver> MotorDrivers { get; set; }
-        public DbSet<MotorCar> MotorCar { get; set; }
+        public DbSet<MotorCar> MotorCars { get; set; }
         public DbSet<MotorRace> MotorRaces { get; set; }
 
 
