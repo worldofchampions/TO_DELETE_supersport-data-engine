5e793cc3fd3ba3969322df4723c5b958cdf580a7
Thobani_Madonsela <thobani@immedia.co.za>
2018-02-01 11:25:54 +0200

    Add mapper profile for player stats model

---
+++ SuperSportDataEngine.Application.WebApi.LegacyFeed/Models/Mappers/LegacyRugbyPointScorerMapperProfile.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+
+namespace SuperSportDataEngine.Application.WebApi.LegacyFeed.Models.Mappers
+{
+    using SuperSportDataEngine.Application.WebApi.LegacyFeed.Models.Rugby;
+    using SuperSportDataEngine.ApplicationLogic.Boundaries.Repository.EntityFramework.PublicSportData.Models;
+
+    public class LegacyRugbyPointScorerMapperProfile : Profile
+    {
+        public LegacyRugbyPointScorerMapperProfile()
+        {
+            CreateMap<RugbyPlayerStatistics, RugbyPointsScorerModel>()
+
+                .ForMember(dest => dest.Tries, exp => exp.MapFrom(src => src.TriesScored))
+
+                .ForMember(dest => dest.Conversions, exp => exp.MapFrom(src => src.ConversionsScored))
+
+                .ForMember(dest => dest.DropGoals, exp => exp.MapFrom(src => src.DropGoalsScored))
+
+                .ForMember(dest => dest.Penalties, exp => exp.MapFrom(src => src.PenaltiesScored))
+
+                .ForMember(dest => dest.Rank, exp => exp.MapFrom(src => src.Rank))
+
+                .ForMember(dest => dest.TotalPoints, exp => exp.MapFrom(src => src.TotalPoints))
+
+                .ForMember(dest => dest.LeagueName, exp => exp.MapFrom(
+                    src => src.RugbyTournament.NameCmsOverride ?? src.RugbyTournament.Name))
+
+                .ForMember(dest => dest.LeagueUrlName, exp => exp.MapFrom(src => src.RugbyTournament.Slug))
+
+                .ForMember(dest => dest.TeamId, exp => exp.MapFrom(src => src.RugbyTeam.LegacyTeamId))
+
+                .ForMember(dest => dest.TeamName, exp => exp.MapFrom(src => src.RugbyTeam.NameCmsOverride))
+
+                .ForAllOtherMembers(dest => dest.Ignore());
+        }
+    }
+}
