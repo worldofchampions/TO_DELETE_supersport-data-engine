a3b6f9f368322267214895b0f772f2b85714816d
Ronald Mariah <ronald@immedia.co.za>
2018-02-07 15:42:12 +0200

    Group 1.

--- SuperSportDataEngine.ApplicationLogic/Services/RugbyIngestWorkerService.cs
+++ SuperSportDataEngine.ApplicationLogic/Services/RugbyIngestWorkerService.cs
@@ -1452,7 +1452,7 @@
 
                     // "GroupStandings" are GroupHierarchyLevel: 1.
                     if (logs.RugbyGroupedLogs.groupStandings != null)
-                        await IngestStandingsForSuperRugby(cancellationToken, 2, logs, logs.RugbyGroupedLogs.groupStandings.ladderposition);
+                        await IngestStandingsForSuperRugby(cancellationToken, 1, logs, logs.RugbyGroupedLogs.groupStandings.ladderposition);
                 }
             }
 
