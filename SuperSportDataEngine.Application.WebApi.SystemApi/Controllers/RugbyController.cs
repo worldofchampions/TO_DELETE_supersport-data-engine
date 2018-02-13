1133ff3dba00df2452fda1367f2f2c612b1fc0f4
Ife Ologbese <ologbesei@yahoo.com>
2018-02-13 15:22:22 +0200

    move appsettings to external config file

--- SuperSportDataEngine.Application.WebApi.SystemApi/Controllers/RugbyController.cs
+++ SuperSportDataEngine.Application.WebApi.SystemApi/Controllers/RugbyController.cs
@@ -2,8 +2,6 @@
 using SuperSportDataEngine.ApplicationLogic.Boundaries.CmsLogic.Interfaces;
 using SuperSportDataEngine.ApplicationLogic.Entities.SystemAPI;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
