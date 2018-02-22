5278d84fda7d29fe095ef75b21fb88d61ef17077
Ife Ologbese <ologbesei@yahoo.com>
2018-02-22 11:20:39 +0200

    added UnityConfig for consistency
    Don't think the file is being used anywhere but added for consistency with LegacyFeed

---
+++ SuperSportDataEngine.Application.WebApi.SystemApi/App_Start/UnityConfig.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace SuperSportDataEngine.Application.WebApi.SystemApi.App_Start
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+            var container = new UnityContainer();
+
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+
+            // e.g. container.RegisterType<ITestService, TestService>();
+
+            GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
+        }
+    }
+}
