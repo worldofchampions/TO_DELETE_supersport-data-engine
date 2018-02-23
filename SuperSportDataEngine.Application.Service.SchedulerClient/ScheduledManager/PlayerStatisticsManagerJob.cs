4e7c13c1aec84f45d08c1a1ea494f657c8b9bb99
Ronald Mariah <ronald@immedia.co.za>
2018-02-23 18:59:00 +0200

    Using the getter method thats used for logs manager.

--- SuperSportDataEngine.Application.Service.SchedulerClient/ScheduledManager/PlayerStatisticsManagerJob.cs
+++ SuperSportDataEngine.Application.Service.SchedulerClient/ScheduledManager/PlayerStatisticsManagerJob.cs
@@ -20,6 +20,7 @@ namespace SuperSportDataEngine.Application.Service.SchedulerClient.ScheduledMana
         private IRecurringJobManager _recurringJobManager;
         private IUnityContainer _childContainer;
         ILoggingService _logger;
+        private static int _maxNumberOfRecentFixturesToConsider;
 
         public PlayerStatisticsManagerJob(
             IRecurringJobManager recurringJobManager,
@@ -27,6 +28,8 @@ namespace SuperSportDataEngine.Application.Service.SchedulerClient.ScheduledMana
         {
             _recurringJobManager = recurringJobManager;
             _childContainer = childContainer;
+            _maxNumberOfRecentFixturesToConsider =
+                int.Parse(ConfigurationManager.AppSettings["MaxNumberOfRecentFixturesToConsider"]);
         }
 
         public async Task DoWorkAsync()
@@ -54,8 +57,14 @@ namespace SuperSportDataEngine.Application.Service.SchedulerClient.ScheduledMana
 
         private async Task CreateAndDeleteChildJobsForFetchingPlayerStatistics()
         {
+            var today = DateTime.UtcNow.Date;
+            var now = DateTime.UtcNow;
+
             var todayTournaments =
-                (await _childContainer.Resolve<IRugbyService>().GetTournamentsForJustEndedFixtures()).ToList();
+                (await _childContainer.Resolve<IRugbyService>().GetRecentResultsFixtures(_maxNumberOfRecentFixturesToConsider))
+                .Where(f => f.StartDateTime.Date == today)
+                .Select(f => f.RugbyTournament)
+                .ToList();
 
             var todayTournamentIds = todayTournaments.Select(t => t.ProviderTournamentId);
 
