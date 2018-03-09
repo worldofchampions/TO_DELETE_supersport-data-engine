3ae423d6eb3bcf030a93a02fbc3fc0f3299698a7
Ronald Mariah <ronald@immedia.co.za>
2018-03-09 15:34:37 +0200

    Updated mapping for grid/eventId endpoint.

--- SuperSportDataEngine.Application.WebApi.LegacyFeed/Models/Mappers/Motorsport/LegacyMotorsportGridMapperProfile.cs
+++ SuperSportDataEngine.Application.WebApi.LegacyFeed/Models/Mappers/Motorsport/LegacyMotorsportGridMapperProfile.cs
@@ -15,11 +15,51 @@ namespace SuperSportDataEngine.Application.WebApi.LegacyFeed.Models.Mappers.Moto
         {
             CreateMap<MotorsportRaceEventGrid, GridModel>()
                 .ForMember(dest => dest.StartInPit, expression => expression.UseValue(false))
+
                 .ForMember(dest => dest.Position, expression => expression.MapFrom(
                     src => src.GridPosition))
+
                 .ForMember(dest => dest.PositionText, expression => expression.UseValue((string) null))
+
                 .ForMember(dest => dest.Time, expression => expression.MapFrom(
                     src => GetTime(src)))
+
+                .ForMember(dest => dest.Points, expression => expression.UseValue((string) null))
+
+                .ForMember(dest => dest.Id, expression => expression.MapFrom(
+                    src => src.MotorsportDriver.LegacyDriverId))
+
+                .ForMember(dest => dest.FirstName, expression => expression.MapFrom(
+                    src => src.MotorsportDriver.FirstName))
+                
+                .ForMember(dest => dest.LastName, expression => expression.MapFrom(
+                    src => src.MotorsportDriver.LastName))
+                
+                .ForMember(dest => dest.Initials, expression => expression.UseValue(""))
+                
+                .ForMember(dest => dest.Abbreviation, expression => expression.UseValue(""))
+
+                .ForMember(dest => dest.FullName, expression => expression.MapFrom(
+                    src => src.MotorsportDriver.FullNameCmsOverride ?? src.MotorsportDriver.FirstName + " " + src.MotorsportDriver.LastName))
+
+                .ForMember(dest => dest.CarNumber, src => src.UseValue(""))
+
+                .ForMember(dest => dest.Country, expression => expression.MapFrom(
+                    src => src.MotorsportDriver.CountryName))
+
+                .ForMember(dest => dest.TeamId, expression => expression.MapFrom(
+                    src => src.MotorsportTeam.LegacyTeamId))
+
+                .ForMember(dest => dest.TeamName, expression => expression.MapFrom(
+                    src => src.MotorsportTeam.NameCmsOverride ?? src.MotorsportTeam.Name))
+
+                .ForMember(dest => dest.TeamShortName, expression => expression.MapFrom(
+                    src => src.MotorsportTeam.NameCmsOverride ?? src.MotorsportTeam.Name))
+
+                .ForMember(dest => dest.TeamCompetition, expression => expression.UseValue((string) null))
+
+                .ForMember(dest => dest.TeamCompetitionId, expression => expression.UseValue(0))
+
                 .ForAllOtherMembers(m => m.Ignore());
         }
 
