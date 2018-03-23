55259f4783fdc91f09b8fb3489da742d7cd13e2a
Ronald Mariah <ronald@immedia.co.za>
2018-03-23 16:47:04 +0200

    XML Attribute on the class name. This is so that when serialising the object to XML we can use the defined root name.

--- SuperSportDataEngine.Application.WebApi.LegacyFeed/Models/Motorsport/MotorsportGrid.cs
+++ SuperSportDataEngine.Application.WebApi.LegacyFeed/Models/Motorsport/MotorsportGrid.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Xml.Serialization;
 
 namespace SuperSportDataEngine.Application.WebApi.LegacyFeed.Models.Motorsport
 {
+    [XmlRoot("motorsportGrid")]
     public class MotorsportGrid
     {
         public List<Grid> RaceGrid { get; set; }
--- SuperSportDataEngine.Application.WebApi.LegacyFeed/Models/Motorsport/MotorsportResult.cs
+++ SuperSportDataEngine.Application.WebApi.LegacyFeed/Models/Motorsport/MotorsportResult.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace SuperSportDataEngine.Application.WebApi.LegacyFeed.Models.Motorsport
 {
+    [XmlRoot("motorsportResult")]
     public class MotorsportResult
     {
         public List<ResultMotorsport> RaceResult { get; set; }
