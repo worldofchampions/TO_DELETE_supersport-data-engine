a20e4ebefb34b062596dfa4c5073fbb87f11ca6c
Thobani_Madonsela <thobani@immedia.co.za>
2018-04-19 09:23:11 +0200

    Removed unused code

--- SuperSportDataEngine.ApplicationLogic/Boundaries/Gateway.Http.Stats/Models/Motorsport/ApiResult.cs
+++ SuperSportDataEngine.ApplicationLogic/Boundaries/Gateway.Http.Stats/Models/Motorsport/ApiResult.cs
@@ -26,7 +26,6 @@
         public string abbreviation { get; set; }
         public string displayName { get; set; }
         public SubLeague subLeague { get; set; }
-        // public List<UriPath> uriPaths { get; set; }
         public Season season { get; set; }
         public List<Race> races { get; set; }
         public List<UriPath2> uriPaths { get; set; }
@@ -51,7 +50,6 @@
         public League2 league { get; set; }
         public List<Season> seasons { get; set; }
     }
-
 
     public class Season
     {
