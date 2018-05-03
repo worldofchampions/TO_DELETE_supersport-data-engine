6c3b200dd7c3e1a486dd11435e20c17986f5cc52
Ife Ologbese <ologbesei@yahoo.com>
2018-05-03 12:30:34 +0200

    update to use string interpolation

--- SuperSportDataEngine.ApplicationLogic/Services/Cms/RugbyCmsService.cs
+++ SuperSportDataEngine.ApplicationLogic/Services/Cms/RugbyCmsService.cs
@@ -279,10 +279,10 @@ namespace SuperSportDataEngine.ApplicationLogic.Services.Cms
             if (tournamentFixtures.Results.Any())
             {
                 if (seasonId != null && tournamentFixtures.NextPageUrl != null)
-                    tournamentFixtures.NextPageUrl += string.Format("&seasonId={0}", seasonId);
+                    tournamentFixtures.NextPageUrl += $"&seasonId={seasonId}";
 
                 if (status != null && tournamentFixtures.NextPageUrl != null)
-                    tournamentFixtures.NextPageUrl += string.Format("&status={0}", status);
+                    tournamentFixtures.NextPageUrl += $"&status={status}";
 
                 return tournamentFixtures;
             }
