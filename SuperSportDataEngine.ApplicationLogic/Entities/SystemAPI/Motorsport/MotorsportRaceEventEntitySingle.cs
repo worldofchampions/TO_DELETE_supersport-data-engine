64e65612cb961f6a6e5546af01c2b51a2a68378c
Ife Ologbese <ologbesei@yahoo.com>
2018-06-19 11:53:13 +0200

    manage motorsport race events
    => List motorsport race events
    => Retrieve single race events
    => Update motorsport race events

---
+++ SuperSportDataEngine.ApplicationLogic/Entities/SystemAPI/Motorsport/MotorsportRaceEventEntitySingle.cs
@@ -0,0 +1,43 @@
+﻿using SuperSportDataEngine.ApplicationLogic.Boundaries.Repository.EntityFramework.Common.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperSportDataEngine.ApplicationLogic.Entities.SystemAPI.Motorsport
+{
+    public class MotorsportRaceEventEntitySingle
+    {
+        /// <summary>
+        /// The motorsport race event.
+        /// </summary>
+        public MotorsportRaceEventEntity RaceEvent { get; set; }
+        /// <summary>
+        /// List of available race event statuses.
+        /// </summary>
+        public List<EnumValue> MotorsportRaceEventStatuses
+        {
+            get
+            {
+                List<EnumValue> values = new List<EnumValue>();
+                foreach (var itemType in Enum.GetValues(typeof(MotorsportRaceEventStatus)))
+                {
+                    //For each value of this enumeration, add a new EnumValue instance
+                    values.Add(new EnumValue()
+                    {
+                        Name = Enum.GetName(typeof(MotorsportRaceEventStatus), itemType),
+                        Value = (int)itemType
+                    });
+                }
+                return values;
+            }
+        }
+    }
+
+    public class EnumValue
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+    }
+}
