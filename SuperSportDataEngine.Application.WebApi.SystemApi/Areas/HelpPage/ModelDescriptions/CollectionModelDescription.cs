6206039c65521956d3235c5f9f65989ec0c601e7
Ronald Mariah <ronald@immedia.co.za>
2018-08-22 14:01:15 +0200

    Updates.

--- SuperSportDataEngine.Application.WebApi.SystemApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++ SuperSportDataEngine.Application.WebApi.SystemApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -1,13 +1,7 @@
 namespace SuperSportDataEngine.Application.WebApi.SystemApi.Areas.HelpPage.ModelDescriptions
 {
-    /// <summary>
-    /// 
-    /// </summary>
     public class CollectionModelDescription : ModelDescription
     {
-        /// <summary>
-        /// 
-        /// </summary>
         public ModelDescription ElementDescription { get; set; }
     }
 }
