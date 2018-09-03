182c010799f03c5035a52882b8e59aa925bad763
Ronald Mariah <ronald@immedia.co.za>
2018-09-03 21:28:16 +0200

    Added seeding for DefaultRugbyLogType in RugbyTournaments table. Updated Ingest to correctly set this new value for new RugbySeasons.

---
+++ SuperSportDataEngine.Repository.EntityFramework.PublicSportData/Migrations/Seed/SeedRugbyDefaultLogTypeTournaments.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using SuperSportDataEngine.ApplicationLogic.Boundaries.Repository.EntityFramework.PublicSportData.Models.Enums;
+using SuperSportDataEngine.Repository.EntityFramework.PublicSportData.Context;
+
+namespace SuperSportDataEngine.Repository.EntityFramework.PublicSportData.Migrations.Seed
+{
+    public static class SeedRugbyDefaultLogTypeTournaments
+    {
+        public static void Seed(PublicSportDataContext context)
+        {
+            try
+            {
+                var tournamentsSlugs = GetGroupedLogTournamentSlugNames();
+                foreach (var tournamentsSlug in tournamentsSlugs)
+                {
+                    var tournament = context.RugbyTournaments.First(t => t.Slug == tournamentsSlug);
+                    tournament.DefaultRugbyLogType = RugbyLogType.GroupedLogs;
+                }
+
+                context.SaveChanges();
+            }
+            catch (Exception exception)
+            {
+                // TODO: Add logging.
+                Console.WriteLine(exception);
+            }
+        }
+
+        private static List<string> GetGroupedLogTournamentSlugNames()
+        {
+            var tournaments = new List<string>
+            {
+                "super-rugby",
+                "supersport-challenge",
+                "junior-rugby",
+                "new-zealand",
+                "pro14",
+                "champions-cup",
+                "sevens"
+            };
+
+            return tournaments;
+        }
+    }
+}
