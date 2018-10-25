eaf83b6312f8a262a2e0e7e8346e7c6cb1f58c62
Ronald Mariah <ronald@immedia.co.za>
2018-10-25 13:46:40 +0200

    Added scripts for creating VM's from a base image.

---
+++ Azure.Powershell.Scripts/staging_create_scheduler_client_vm.ps1
@@ -0,0 +1,45 @@
+# Login to Azure.
+Login-AzureRmAccount
+
+# Use the SuperSport subscription.
+Set-AzureRmContext -SubscriptionId "1b355cd4-4b64-4890-a31b-840bafbb63b1"
+
+$deploymentResourceGroup = "SuperSport-SSDE-Staging"
+$vmName = "awe-ssde-s-sc"
+$vmSize = "Standard_F4s_v2"
+
+# Create a new Network Security Group which 
+# allows port 9622 to be opened and accessed.
+
+$httprule = New-AzureRmNetworkSecurityRuleConfig `
+    -Name "allow_hangfire_dashboard_access" `
+    -Description "Allow HTTP" `
+    -Access "Allow" `
+    -Protocol "Tcp" `
+    -Direction "Inbound" `
+    -Priority "100" `
+    -SourceAddressPrefix "*" `
+    -SourcePortRange * `
+    -DestinationAddressPrefix * `
+    -DestinationPortRange 9622
+
+$nsg = New-AzureRmNetworkSecurityGroup `
+    -ResourceGroupName $deploymentResourceGroup `
+    -Location "West Europe" `
+    -Name $vmName `
+    -SecurityRules $httprule
+
+# Create the VM in the existing Virtual Network.
+New-AzureRmResourceGroupDeployment `
+    -Name "deployment" `
+    -ResourceGroupName $deploymentResourceGroup `
+    -TemplateUri 201-vm-different-rg-vnet/azuredeploy.json `
+    -vmName $vmName `
+    -newStorageAccountName "awessdesstorage" `
+    -adminUsername "SuperSportDataEngine" `
+    -virtualNetworkName "SuperSport-API-Vnet" `
+    -virtualNetworkResourceGroup "SuperSport-API-Backend-Prod" `
+    -subnet1Name "default" `
+    -nicName $vmName `
+    -vmSize $vmSize `
+    -publicIPName $vmName
